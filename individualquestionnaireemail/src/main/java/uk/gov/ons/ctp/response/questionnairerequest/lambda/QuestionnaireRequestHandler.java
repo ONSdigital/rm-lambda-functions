69dbb970c03028d84b788912e015240f778879c9
Phil Whiles <phil.whiles@gmail.com>
2017-01-17 11:27:12 +0000

    CTPA-1029 added encryption to copy

--- individualquestionnaireemail/src/main/java/uk/gov/ons/ctp/response/questionnairerequest/lambda/QuestionnaireRequestHandler.java
+++ individualquestionnaireemail/src/main/java/uk/gov/ons/ctp/response/questionnairerequest/lambda/QuestionnaireRequestHandler.java
@@ -8,7 +8,9 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
+import com.amazonaws.services.s3.model.CopyObjectRequest;
 import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.S3Object;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -16,9 +18,15 @@ import uk.gov.ons.ctp.response.questionnairerequest.model.QuestionnaireRequest;
 import uk.gov.ons.ctp.response.questionnairerequest.service.NotifyService;
 
 public class QuestionnaireRequestHandler implements RequestHandler<S3Event, Object> {
-  
+
   public static final String PROCESSED_POSTFIX = ".processed";
 
+  /**
+   * AWS Lambda function to receive AWS S3 Event on json file creation.
+   * Reads the file and uses contents to construct an email that is sent to UK.GOV Notify Service
+   * @param input the S3Event - indicates the bucket and file name
+   * @param context the AWS Context for the invocation, giving access to the logger 
+   */
   public Object handleRequest(S3Event input, Context context) {
     ObjectMapper mapper = new ObjectMapper();
     AmazonS3Client s3Client = new AmazonS3Client();
@@ -26,7 +34,7 @@ public class QuestionnaireRequestHandler implements RequestHandler<S3Event, Obje
     for (S3EventNotificationRecord record : input.getRecords()) {
       String bucketName = record.getS3().getBucket().getName();
       String key = record.getS3().getObject().getKey();
-      context.getLogger().log("Bucket:" + bucketName + " Key:"+key);
+      context.getLogger().log("Bucket:" + bucketName + " Key:" + key);
 
       S3Object object = s3Client.getObject(
           new GetObjectRequest(bucketName, key));
@@ -35,8 +43,7 @@ public class QuestionnaireRequestHandler implements RequestHandler<S3Event, Obje
         QuestionnaireRequest questionnaireRequest = mapper.readValue(inputStream, QuestionnaireRequest.class);
         boolean success = NotifyService.process(questionnaireRequest, context.getLogger());
         if (success) {
-          s3Client.copyObject(bucketName, key, bucketName + PROCESSED_POSTFIX, key);
-          s3Client.deleteObject(bucketName, key);
+          moveObject(s3Client, bucketName, key);
         } else {
           context.getLogger().log("Failed to send notify email :" + questionnaireRequest);
         }
@@ -47,4 +54,16 @@ public class QuestionnaireRequestHandler implements RequestHandler<S3Event, Obje
     return null;
   }
 
+  private void moveObject(AmazonS3Client s3Client, String bucketName, String key) {
+    CopyObjectRequest copyObjRequest = new CopyObjectRequest(
+        bucketName, key, bucketName + PROCESSED_POSTFIX, key);
+
+    // Request server-side encryption.
+    ObjectMetadata objectMetadata = new ObjectMetadata();
+    objectMetadata.setSSEAlgorithm(ObjectMetadata.AES_256_SERVER_SIDE_ENCRYPTION);
+    copyObjRequest.setNewObjectMetadata(objectMetadata);
+
+    s3Client.copyObject(copyObjRequest);
+    s3Client.deleteObject(bucketName, key);
+  }
 }
